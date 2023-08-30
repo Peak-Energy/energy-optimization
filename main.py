603874def0a06e5a844ffbf52446f8f4ea1232cf
Kong You Liow <53225674+kyliow@users.noreply.github.com>
2023-08-30 22:57:18 +0800

    remove data to another file

--- main.py
+++ main.py
@@ -1,102 +1,81 @@
 import gurobipy
 from gurobipy import GRB
 
-energies = [1,2,3]
-quarters = [1,2,3,4]
-
-# Such structure for variable limits
-production_upper_lim = {
-    (1,1): 10000, (1,2): 8000, (1,3): 6000,
-    (2,1): 9000, (2,2): 5000, (2,3): 10000,
-    (3,1): 10000, (3,2): 5000, (3,3): 11000,
-    (4,1): 9000, (4,2): 7000, (4,3): 7000,
-}
-storage_lim = 15000
-
-# Such structure for parameters
-demand = {
-    1: 20000, 2: 30000, 3: 18000, 4: 22000
-}
-
-storage_cost = {
-    1: 2, 2: 0, 3: 5
-}
-
-production_cost = {
-    (1,1): 5, (1,2): 5, (1,3): 3,
-    (2,1): 10, (2,2): 7, (2,3): 5,
-    (3,1): 6, (3,2): 5, (3,3): 2,
-    (4,1): 5, (4,2): 6, (4,3): 2,
-}
+from data_extraction import Data
 
 # Model
-model = gurobipy.Model('Energy optimisation')
+model = gurobipy.Model("Energy optimisation")
 
 # Variables
-X = model.addVars(quarters, energies, name='Amount supplied', ub=production_upper_lim)
-Y = model.addVars(quarters, energies, name='Amount remained')
-Z = model.addVars(quarters, energies, name='Amount used')
+X = model.addVars(
+    Data.months, Data.energies, name="Amount supplied", ub=Data.production_limit
+)
+Y = model.addVars(Data.months, Data.energies, name="Amount remained")
+Z = model.addVars(Data.months, Data.energies, name="Amount used")
 
 # Constraints
 # Energy balance
 model.addConstrs(
     (
-        Y[quarters[q_ind - 1], energy] + X[quarter, energy]
-        == Z[quarter, energy] + Y[quarter, energy]
-        for energy in energies for q_ind, quarter in enumerate(quarters)
-        if quarter != quarters[0]
+        Y[Data.months[m_ind - 1], energy] + X[month, energy]
+        == Z[month, energy] + Y[month, energy]
+        for energy in Data.energies
+        for m_ind, month in enumerate(Data.months)
+        if month != Data.months[0]
     ),
-    name="Energy balance"
+    name="Energy balance",
 )
 model.addConstrs(
     (
-        X[quarters[0], energy]
-        == Z[quarters[0], energy] + Y[quarters[0], energy]
-        for energy in energies
+        X[Data.months[0], energy]
+        == Z[Data.months[0], energy] + Y[Data.months[0], energy]
+        for energy in Data.energies
     ),
-    name="Energy balance"
+    name="Energy balance",
 )
 
 # Demand
 model.addConstrs(
     (
         gurobipy.quicksum(
-        Y[quarters[q_ind - 1], energy] + X[quarter, energy] for energy in energies
-        ) >= demand[quarter] 
-        for q_ind, quarter in enumerate(quarters) 
-        if quarter != quarters[0]
+            Y[Data.months[m_ind - 1], energy] + X[month, energy]
+            for energy in Data.energies
+        )
+        >= Data.demand[month]
+        for m_ind, month in enumerate(Data.months)
+        if month != Data.months[0]
     ),
-    name='Demand'
+    name="Demand",
 )
 model.addConstr(
     (
-        gurobipy.quicksum(
-        X[quarters[0], energy] for energy in energies
-        ) >= demand[quarters[0]] 
+        gurobipy.quicksum(X[Data.months[0], energy] for energy in Data.energies)
+        >= Data.demand[Data.months[0]]
     ),
-    name='Demand'
+    name="Demand",
 )
 
-# Storage 
+# Storage
 model.addConstrs(
     (
-        gurobipy.quicksum(
-            Y[quarter, energy] for energy in energies
-        ) <= storage_lim for quarter in quarters
+        Y[month, energy] <= Data.storage_limit[energy]
+        for month in Data.months
+        for energy in Data.energies
     ),
-    name='Storage'
+    name="Storage",
 )
 
 # Objective function
 obj = gurobipy.quicksum(
-    (production_cost[quarter, energy]*X[quarter, energy])
-    + (storage_cost[energy]*Y[quarter, energy])
-    for energy in energies 
-    for quarter in quarters 
+    (Data.production_cost[energy] * X[month, energy])
+    + (Data.storage_cost[energy] * Y[month, energy])
+    for energy in Data.energies
+    for month in Data.months
 )
 model.setObjective(obj, GRB.MINIMIZE)
 
 # Run
 model.optimize()
-model.printAttr('X')
+model.printAttr("X")
 
+model.write("out.json")
